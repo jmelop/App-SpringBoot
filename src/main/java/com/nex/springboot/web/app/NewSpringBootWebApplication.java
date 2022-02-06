0491b811d5fde4f9c7b9aed91e7848e3e8145d1c
Juan Melo Pérez <juanmlstm@gmail.com>
2022-02-06 19:01:10 +0000

    First commit

---
+++ src/main/java/com/nex/springboot/web/app/NewSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.nex.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nex/springboot/web/app/NewSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nex.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
